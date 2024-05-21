95e2e52de826f47eb9bc2ba8d95e3e14594e52d2
Alice K. <alice.khoudli@polytechnique.org>
2024-05-21 02:23:08 +0200

    frontend : add wip admin index page with sidebar component

---
+++ frontend/components/admin/Sidebar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Tree
+      :value="nodes"
+      class="w-full md:w-30rem"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import state from '~/lib/admin-state'
+
+// TODO : add proper @nodeSelect="onNodeSelect" to each node to enable functionality
+await state.fetchFamilies()
+const family_nodes = state.families.map(item => ({
+  label: item.title,
+  key: item.id,
+  children: [
+    {
+      label: 'Entities',
+    },
+    {
+      label: 'Comments',
+    },
+  ],
+}))
+
+const nodes = [
+  {
+    label: 'Accueil',
+  },
+  {
+    label: 'General Config',
+  },
+  {
+    label: 'Users',
+  },
+  {
+    label: 'Access Tokens',
+  },
+  {
+    label: 'Families & Forms',
+  },
+  ...family_nodes,
+  {
+    label: 'Tags',
+  },
+]
+</script>
---
+++ frontend/pages/admin/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+      navbar
+    </div>
+    <div>
+      <div>
+        <AdminSidebar />
+      </div>
+      <div>
+        mainpanel
+      </div>
+      <div>
+        filterbox
+      </div>
+    </div>
+  </div>
+</template>
