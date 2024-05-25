97614bec293ead0ba837cd287b41de2e35ed6f1d
Alice K. <alice.khoudli@polytechnique.org>
2024-05-26 01:25:30 +0200

    frontend: add custom confirmation dialog wrapper component

---
+++ frontend/components/ConfirmationDialog.vue
@@ -0,0 +1,61 @@
+<template>
+  <ConfirmPopup group="delete">
+    <template #message="slotProps">
+      <div class="flex flex-row align-items-center w-full gap-2 p-3 mb-2 pb-0">
+        <AppIcon
+          :icon-name="slotProps.message.icon!"
+        />
+        <span>{{ slotProps.message.message }} <b>{{ (slotProps.message as ExtendedConfirmationOptions).objectId }}</b></span>
+      </div>
+    </template>
+  </ConfirmPopup>
+</template>
+
+<script setup lang="ts">
+import type { ConfirmationOptions } from 'primevue/confirmationoptions'
+import AppIcon from '~/components/AppIcon.vue'
+
+const props = withDefaults(defineProps<{
+  message?: string
+  objectDescriptor?: string
+  icon?: string
+  rejectLabel?: string
+  acceptLabel?: string
+  onAccept: () => void
+  onReject: () => void
+}>(),
+{
+  message: 'Confirmer la suppression de ',
+  objectDescriptor: '',
+  icon: 'warning',
+  rejectLabel: 'Annuler',
+  acceptLabel: 'Confirmer',
+  onAccept: () => {},
+  onReject: () => {},
+})
+
+const confirm = useConfirm()
+interface ExtendedConfirmationOptions extends ConfirmationOptions {
+  objectId?: string
+}
+
+function displayDialog(event: Event, objectId: string) {
+  // if id==
+  const options: ExtendedConfirmationOptions = {
+    target: event.currentTarget as HTMLElement,
+    group: 'delete',
+    message: `${props.message}${props.objectDescriptor}`,
+    objectId: objectId,
+    icon: props.icon,
+    rejectClass: 'p-button-secondary p-button-outlined p-button-sm',
+    acceptClass: 'p-button-sm',
+    rejectLabel: props.rejectLabel,
+    acceptLabel: props.acceptLabel,
+    reject: props.onReject,
+    accept: props.onAccept,
+  }
+  confirm.require(options)
+}
+
+defineExpose({ displayDialog })
+</script>
