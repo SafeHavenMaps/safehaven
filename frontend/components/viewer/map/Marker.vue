56765cdaf76eab0654d719a873782a48fbcdc9d5
Alice K. <alice.khoudli@polytechnique.org>
2024-06-29 00:50:11 +0200

    frontend: highlighted marker no longer blur

--- frontend/components/viewer/map/Marker.vue
+++ frontend/components/viewer/map/Marker.vue
@@ -1,13 +1,24 @@
 <template>
-  <img
-    :class="{ highlighted: !!props.highlighted }"
-    :width="props.width"
-    :height="props.height"
-    :src="url"
-    style="cursor: pointer"
-    class="map-marker"
-    @click="handleClick"
+  <div
+    class="map-marker-container"
+    :style="
+      { width: `${props.highlighted ? Math.round(1.25*props.width) : props.width}px`,
+        height: `${props.highlighted ? Math.round(1.25*props.height) : props.height}px` }"
   >
+    <img
+      :class="{ highlighted: !!props.highlighted }"
+      :width="props.highlighted ? Math.round(1.25*props.width) : props.width"
+      :height="props.highlighted ? Math.round(1.25*props.height) : props.height"
+      :src="props.highlighted ? urls.upscaled_url : urls.url"
+      style="cursor: pointer"
+      class="map-marker"
+      @click="handleClick"
+    >
+    <img
+      hidden
+      :src="urls.upscaled_url"
+    >
+  </div>
 </template>
 
 <script setup lang="ts" generic="T">
@@ -21,20 +32,22 @@ const props = defineProps<{
   callbackItem: T
 }>()
 
-const url = computed(() => {
-  let url = `/api/icons/pin?h=${props.height}&w=${props.width}`
+const urls = computed(() => {
+  const url = `/api/icons/pin?h=${props.height}&w=${props.width}`
+  const upscaled_url = `/api/icons/pin?h=${Math.round(1.25 * props.height)}&w=${Math.round(1.25 * props.width)}`
 
+  let url_end = ''
   if (props.iconHash) {
-    url += `&ih=${props.iconHash}`
+    url_end += `&ih=${props.iconHash}`
   }
   if (props.borderColor) {
-    url += `&bc=${props.borderColor.replace('#', '')}`
+    url_end += `&bc=${props.borderColor.replace('#', '')}`
   }
   if (props.fillColor) {
-    url += `&fc=${props.fillColor.replace('#', '')}`
+    url_end += `&fc=${props.fillColor.replace('#', '')}`
   }
 
-  return url
+  return { url: url + url_end, upscaled_url: upscaled_url + url_end }
 })
 
 const emit = defineEmits<{
@@ -47,21 +60,27 @@ const handleClick = () => {
 </script>
 
 <style scoped>
+.map-marker-container {
+  position: relative;
+}
+
 .map-marker {
-  transform: translateX(-50%) translateY(-100%);
+  display: block;
+  position: absolute;
+  top: -100%;
+  left: -50%;
 }
 
 .map-marker.highlighted {
-  transform: translateX(-50%) translateY(-100%) scale(1.25);
   animation: bounce 0.75s infinite alternate;
 }
 
 @keyframes bounce {
   0% {
-    transform: translateY(-100%) scale(1.25) translateX(-50%);
+    transform: translateY(0%);
   }
   100% {
-    transform: translateY(-150%) scale(1.25) translateX(-50%);
+    transform: translateY(-50%);
   }
 }
 </style>
