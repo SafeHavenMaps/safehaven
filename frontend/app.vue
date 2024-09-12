ae65cab1b7591cebe8ad65b314ec0ab9c2a7b9ca
Alice K. <alice.khoudli@polytechnique.org>
2024-09-12 18:33:32 +0200

    frontend: add confirmation dialog when deselecting include comment in user entity addition

--- frontend/app.vue
+++ frontend/app.vue
@@ -12,9 +12,19 @@
   </NuxtLayout>
 
   <Toast />
+
+  <ConfirmPopup group="delete">
+    <template #message="slotProps">
+      <div class="flex flex-row items-center w-full gap-2 p-4 mb-2 pb-0">
+        <AppIcon :icon-name="slotProps.message.icon!" />
+        <span>{{ slotProps.message.message }} <b>{{ (slotProps.message as ExtendedConfirmationOptions).objectId }}</b></span>
+      </div>
+    </template>
+  </ConfirmPopup>
 </template>
 
 <script setup lang="ts">
+import type { ConfirmationOptions } from 'primevue/confirmationoptions'
 import { useHead } from '#imports'
 import state from '~/lib/viewer-state'
 
@@ -28,4 +38,8 @@ useHead({
     href: state.logo ?? '/default_favicon.ico',
   }],
 })
+
+interface ExtendedConfirmationOptions extends ConfirmationOptions {
+  objectId?: string
+}
 </script>
